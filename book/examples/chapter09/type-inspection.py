d5520fa52eaeaa1282b0bd2c6fcdf80fbc38db12
FilippovVladislav <FilippovVladislav@users.noreply.github.com>
2026-08-11 05:48:33 +0000

    deploy: 968b4081d27ed4652cde3cf6d4d06c64f45f17a3

---
+++ book/examples/chapter09/type-inspection.py
@@ -0,0 +1,11 @@
+age = 25
+height = 1.82
+name = "Анна"
+is_learning = True
+result = None
+
+print(age, type(age))
+print(height, type(height))
+print(name, type(name))
+print(is_learning, type(is_learning))
+print(result, type(result))
