dfd1436657f55e3b43fa789c6750c96158ca8940
FilippovVladislav <vlad.filippov.tmn@gmail.com>
2026-08-19 15:10:13 +0300

    Add value comparisons chapter

---
+++ book/examples/chapter13/comparison-chain.py
@@ -0,0 +1,5 @@
+age = 25
+temperature = 20
+
+print(18 <= age <= 65)
+print(0 <= temperature <= 30)
---
+++ book/examples/chapter13/string-comparison.py
@@ -0,0 +1,8 @@
+print("Python" == "Python")
+print("Python" == "python")
+print("Python" != "Java")
+
+password = "Python123"
+
+print(password == "Python123")
+print(password == "python123")
