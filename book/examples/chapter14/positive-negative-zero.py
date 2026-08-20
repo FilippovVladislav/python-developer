2a2999f18a3544cf56ee7f023a6283580570338c
FilippovVladislav <FilippovVladislav@users.noreply.github.com>
2026-08-20 13:34:43 +0000

    deploy: b48a39fe7f5b0aa8c45c04b701fb23d8c34ae528

---
+++ book/examples/chapter14/positive-negative-zero.py
@@ -0,0 +1,8 @@
+number = float(input("Введите число: "))
+
+if number > 0:
+    print("Положительное число")
+elif number < 0:
+    print("Отрицательное число")
+else:
+    print("Ноль")
---
+++ book/examples/chapter14/temperature-elif.py
@@ -0,0 +1,8 @@
+temperature = float(input("Введите температуру: "))
+
+if temperature > 25:
+    print("Жарко")
+elif temperature > 10:
+    print("Тепло")
+else:
+    print("Холодно")
